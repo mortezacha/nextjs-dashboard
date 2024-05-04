2595577b3ae3cef3f8000186450dd46b6707a43e
mortezacha <mortezachabooshi@gmail.com>
2024-05-04 16:13:02 +0430

    Delete the fetchCardData function

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,7 +3,6 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import { fetchCardData } from '@/app/lib/data';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
