a12f14e2b870cee615562646ff96947af38af8f2
mori_cha <mortezachabooshi@gmail.com>
2024-05-04 11:36:51 +0430

    first push of my dashboard next js app

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -12,13 +12,13 @@ const users = [
 const customers = [
   {
     id: '3958dc9e-712f-4377-85e9-fec4b6a6442a',
-    name: 'Delba de Oliveira',
+    name: 'morteza chabooshi',
     email: 'delba@oliveira.com',
     image_url: '/customers/delba-de-oliveira.png',
   },
   {
     id: '3958dc9e-742f-4377-85e9-fec4b6a6442a',
-    name: 'Lee Robinson',
+    name: 'zahra qiasi',
     email: 'lee@robinson.com',
     image_url: '/customers/lee-robinson.png',
   },
