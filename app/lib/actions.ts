60bab1d929b8038939e8a388b80e6e0b9f6618b6
mortezacha <mortezachabooshi@gmail.com>
2024-05-09 02:59:08 +0430

    change to cent and make a new date for created at

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,14 +12,12 @@ const FormSchema = z.object({
    
   const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData : FormData){
+  export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-      });
-
-    // const rawFormData = Object.fromEntries(formData.entries())
-
-    // console.log(typeof rawFormData.amount);
-}
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+  }
