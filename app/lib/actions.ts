a4aafa9d2ee1cde510aad772045eba86c91e993a
mortezacha <mortezachabooshi@gmail.com>
2024-05-10 03:03:29 +0430

    Inserting the data into your database and revalidating and redirect to table of invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import {z} from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation'; 
 
 const FormSchema = z.object({
     id: z.string(),
@@ -20,4 +23,11 @@ const FormSchema = z.object({
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
   }
