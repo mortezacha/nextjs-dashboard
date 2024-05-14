122de01201b150b555ca7ed3e110bf83e1f8e3ad
mortezacha <mortezachabooshi@gmail.com>
2024-05-15 02:52:51 +0430

    add server side error handling to edit invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -36,7 +36,7 @@ export type State = {
         amount: formData.get('amount'),
         status: formData.get('status'),
       });
-console.log(validatedFields)
+      
         // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
@@ -65,13 +65,19 @@ console.log(validatedFields)
 
   const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-  export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+  export async function updateInvoice(id: string, prevState: State,formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
       customerId: formData.get('customerId'),
       amount: formData.get('amount'),
       status: formData.get('status'),
     });
-   
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Missing Fields. Failed to Create Invoice.',
+      };
+    }
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
    try {
     await sql`
--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -120,13 +120,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               </div>
             </div>
             <div id="status-error" aria-live="polite" aria-atomic="true">
-            {state.errors?.status &&
-              state.errors.status.map((error: string) => (
-                <p className="mt-2 text-sm text-red-500" key={error}>
-                  {error}
-                </p>
-              ))}
-          </div>
+              {state.errors?.status &&
+                state.errors.status.map((error: string) => (
+                  <p className="mt-2 text-sm text-red-500" key={error}>
+                    {error}
+                  </p>
+                ))}
+            </div>
           </div>
         </fieldset>
       </div>
